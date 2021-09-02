93ca58c94fc4de1287e46d126bb56e4975efc19f
lennylxx <lennylxx@gmail.com>
2021-09-02 15:01:34 +0800

    change default build output folder

--- build.sh
+++ build.sh
@@ -5,7 +5,8 @@ rm -rf ~/Library/Input\ Methods/GoogleInputTools.app
 rm -rf ~/Library/Input\ Methods/GoogleInputTools.swiftmodule
 rm -rf ~/Library/Containers/com.lennylxx.inputmethod.GoogleInputTools/
 rm -rf ~/Library/Developer/Xcode/DerivedData/GoogleInputTools-*/
+rm -rf ./build
 
-xcodebuild -scheme GoogleInputTools build
+xcodebuild -scheme GoogleInputTools build CONFIGURATION_BUILD_DIR=/Users/$(id -un)/Library/Input\ Methods/
 
 ls -al ~/Library/Input\ Methods
